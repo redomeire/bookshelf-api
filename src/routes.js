e08c58d7f004c259a466e120094401664422cef9
unijreng <redomeire2@gmail.com>
2022-01-18 20:08:24 +0700

    menambahkan option cors pada routes.js

--- src/routes.js
+++ src/routes.js
@@ -17,21 +17,41 @@ const routes = [
     method: 'GET',
     path: '/books',
     handler: showAllBooksHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   },
   {
     method: 'GET',
     path: '/books/{bookId}',
     handler: getBookByIdHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   },
   {
     method: 'PUT',
     path: '/books/{bookId}',
     handler: updateByIdHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   },
   {
     method: 'DELETE',
     path: '/books/{bookId}',
     handler: deleteByIdHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   },
 ];
 
